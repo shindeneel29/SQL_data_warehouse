c909fbdd57dee3759d700f35cd8edd1ca7ba1918
shindeneel29 <shindeneel29@gmail.com>
2026-03-06 20:17:49 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,17 @@
+--Created Databse Datawarehouse and created schemas Bronze, Silver and Gold
+
+use master
+
+create database Datawarehouse;
+Go
+
+use Datawarehouse
+
+Create Schema Bronze
+Go
+
+Create Schema Silver
+Go
+
+Create Schema Gold
+Go
